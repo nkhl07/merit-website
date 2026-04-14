cd902e7e98eea3c40a5718d1ef8e7568cb747c17
Maxwell Tucker <132511790+mtcyber10@users.noreply.github.com>
2026-04-14 13:24:43 -0400

    Fix link to open form in new tab
    Updated the link to open a form in a new tab.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -104,7 +104,9 @@ export default function Navbar() {
             ))}
             <div className="pt-2">
               <Link
-                href="https://forms.gle/XgDAnvKSykMYqgEM6" target="_blank"
+                <a href="https://forms.gle/XgDAnvKSykMYqgEM6" target="_blank">
+                      Open Form
+                </a>
                 className="block w-full text-center px-4 py-3 rounded-lg text-sm font-semibold bg-maroon text-white hover:bg-maroon-900 transition-colors"
               >
                 Join MERIT
